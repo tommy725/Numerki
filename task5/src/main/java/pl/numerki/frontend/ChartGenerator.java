29008d8419413de04a5031ebfbf25d705d04b522
Tommy <afteryou725@gmail.com>
2022-05-01 18:52:17 +0200

    Things required in task from previous one

---
+++ task5/src/main/java/pl/numerki/frontend/ChartGenerator.java
@@ -0,0 +1,88 @@
+package pl.numerki.frontend;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import java.awt.*;
+import java.util.function.Function;
+
+public class ChartGenerator {
+    public static JFreeChart generatePlot(
+            Function<Double, Double> function, Function<Double, Double> interpolated, double leftCompartment,
+            double rightCompartment, double[] nodes
+    ) {
+        XYSeries functionSeries = new XYSeries("Funkcja");
+        XYSeries interpolatedSeries = new XYSeries("Funkcja interpolowana");
+        double unit = (rightCompartment - leftCompartment) / 1000;
+        double minValue = function.apply(leftCompartment);
+        double maxValue = function.apply(leftCompartment);
+        for (double i = leftCompartment; i < rightCompartment; i += unit) {
+            double value = function.apply(i);
+            double valueInterpolated = interpolated.apply(i);
+            interpolatedSeries.add(i, valueInterpolated);
+            functionSeries.add(i, value);
+            if (value < minValue) {
+                minValue = value;
+            }
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        XYSeries nodesSeries = new XYSeries("Węzły");
+        for (double node : nodes) {
+            nodesSeries.add(node, function.apply(node));
+        }
+
+        XYSeries xAxis = new XYSeries("oś X");
+        xAxis.add(leftCompartment, 0);
+        xAxis.add(rightCompartment, 0);
+
+        XYSeriesCollection seriesCollection = new XYSeriesCollection();
+        seriesCollection.addSeries(functionSeries);
+        seriesCollection.addSeries(interpolatedSeries);
+        seriesCollection.addSeries(nodesSeries);
+        seriesCollection.addSeries(xAxis);
+
+        if(leftCompartment * rightCompartment < 0) {
+            xAxis.add(0,0);
+            XYSeries yAxis = new XYSeries("oś Y");
+            yAxis.add(0, minValue);
+            yAxis.add(0, maxValue);
+            yAxis.add(0,0);
+            seriesCollection.addSeries(yAxis);
+        }
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Wykres funckji", "X", "Y", seriesCollection,
+                PlotOrientation.VERTICAL, true, true, false
+        );
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+        changeVisibility(renderer, 0, true);
+        changeVisibility(renderer, 1, true);
+        changeVisibility(renderer, 2, false);
+        formatAxis(renderer, 3);
+        formatAxis(renderer, 4);
+
+        plot.setRenderer(renderer);
+
+        return chart;
+    }
+
+    private static void formatAxis(XYLineAndShapeRenderer renderer, int series) {
+        changeVisibility(renderer, series, true);
+        renderer.setSeriesPaint(series, new Color(0x00, 0x00, 0x00));
+        renderer.setSeriesStroke(series, new BasicStroke(0.5f));
+    }
+
+    private static void changeVisibility(XYLineAndShapeRenderer renderer, int series, boolean displayLine) {
+        renderer.setSeriesLinesVisible(series, displayLine);
+        renderer.setSeriesShapesVisible(series, !displayLine);
+    }
+}
--- task5/src/main/java/pl/numerki/frontend/Main.java
+++ task5/src/main/java/pl/numerki/frontend/Main.java
@@ -1,7 +1,113 @@
 package pl.numerki.frontend;
 
+import org.jfree.chart.ChartUtilities;
+import pl.numerki.backend.Functions;
+import pl.numerki.backend.NewtonCotesQuadrature;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello task 5");
+        System.out.print("Podaj liczbę złożeń: ");
+        Scanner s = new Scanner(System.in);
+        int numOfAssemblies = s.nextInt();
+
+        Function<Double, Double>[] function = new Function[numOfAssemblies];
+        for (int j = 0; j < numOfAssemblies; j++) {
+            System.out.println(getMenu());
+            System.out.print("Wybór: ");
+            int choose = s.nextInt();
+            switch (choose) {
+                case 1 -> function[j] = Functions.sinFunction();
+                case 2 -> {
+                    System.out.print("Podaj a: ");
+                    double a = s.nextDouble();
+                    if (a < 0) {
+                        System.out.println("Podstawa funkcji wykładniczej musi być liczbą dodatnią.");
+                        return;
+                    }
+                    function[j] = Functions.exponentialFunction(a);
+                }
+                case 3 -> {
+                    System.out.print("Podaj stopień wielomianu, który chcesz wprowadzić: ");
+                    int grade = s.nextInt();
+                    if (grade < 0) {
+                        System.out.println("Wybrano nieprawidłowy stopień wielomianu.");
+                        return;
+                    }
+                    double[] factors = new double[grade + 1];
+                    for (int i = 0; i < grade + 1; i++) {
+                        System.out.print("Podaj współczynnik przy " + i + " potędze wielomianu: ");
+                        factors[i] = s.nextDouble();
+                    }
+                    function[j] = Functions.polynomial(factors);
+                }
+                case 4 -> function[j] = Functions.absoluteFunction();
+                default -> {
+                    System.out.println("Wybrano nie prawidłową opcję.");
+                    return;
+                }
+            }
+        }
+        Function<Double, Double> assembledFunction = assemble(function);
+
+        double leftCompartment, rightCompartment;
+
+        System.out.println("Podaj przedział aproksymacji");
+        System.out.print("Początek: ");
+        leftCompartment = s.nextDouble();
+        System.out.print("Koniec: ");
+        rightCompartment = s.nextDouble();
+
+        System.out.print("Podaj stopień wielomianu aproksymującego: ");
+        int polynomialDegree = s.nextInt();
+
+        System.out.print("Podaj dokładność dla metody Newtona-Cotesa: ");
+        double epsilon = s.nextDouble();
+
+        System.out.println(
+                "Kwadratura Newtona-Cotesa: " +
+                        "\n    wynik: " +
+                        NewtonCotesQuadrature.integrate(assembledFunction, leftCompartment, rightCompartment, epsilon) +
+                        "\n    liczba podziałów: " + NewtonCotesQuadrature.numberOfSubCompartments
+        );
+
+//        try {
+//            ChartUtilities.saveChartAsPNG(
+//                    new File("chart.png"),
+//                    ChartGenerator.generatePlot(
+//                            assembledFunction,
+//                            interpolatedFunction,
+//                            leftCompartment,
+//                            rightCompartment,
+//                            nodesX
+//                    ),
+//                    600, 600
+//            );
+//        } catch (IOException e) {
+//            System.out.println("Wystapił problem przy generowaniu wykresu.");
+//        }
+    }
+
+    private static String getMenu() {
+        return """
+                Funckje:\s
+                1 - sin(x)\s
+                2 - a^x\s
+                3 - wielomian (a + bx + cx^2 + ...)\s
+                4 - |x|""";
+    }
+
+    private static Function<Double, Double> assemble(Function<Double, Double>[] array) {
+        return aDouble -> {
+            double result = aDouble;
+            for (Function<Double, Double> doubleDoubleFunction : array) {
+                result = doubleDoubleFunction.apply(result);
+            }
+            return result;
+        };
     }
 }
